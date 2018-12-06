37bcc55940e68cc622a5012b1cd96780f4c23553
Connor Harvey <conbonbot745@gmail.com>
2018-12-06 17:18:07 +0000

    fix email #1

--- app/models/hour.rb
+++ app/models/hour.rb
@@ -3,5 +3,5 @@ class Hour < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   VALID_CONTENT_REGEX = /\A[+-]?\d+\z/ # try later
-  validates :content, presence: true, :inclusion => 1..100
+  validates :content, :inclusion => 1..100
 end
