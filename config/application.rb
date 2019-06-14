078c9a644f245f18c6cf2d4e034242a34d77a762
Connor Harvey <conbonbot745@gmail.com>
2019-06-14 15:58:49 +0000

    flavor

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,10 @@ end
 Bundler.require(*Rails.groups)
 ActionMailer::Base.delivery_method = :smtp
 
+
+# Minimum Sass number precision required by bootstrap-sass
+::Sass::Script::Number.precision = [8, ::Sass::Script::Number.precision].max
+
 module CommunityService
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -17,5 +21,12 @@ module CommunityService
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # Bower asset paths
+    root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
+      config.sass.load_paths << bower_path
+      config.assets.paths << bower_path
+    end
+    # Precompile Bootstrap fonts
+    config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
   end
 end
