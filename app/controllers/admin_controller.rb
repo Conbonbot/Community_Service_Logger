9d744041c779e381449967f795b6343a02d631aa
Connor Harvey <conbonbot745@gmail.com>
2019-07-23 17:39:27 +0000

    changed admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,5 +3,18 @@ class AdminController < ApplicationController
   def home
     @users = User.all
     @supervisors = Supervisor.all
+    @hours = Hour.where(approved: true)
+    @hours_week = @hours.where(updated_at: 1.week.ago.in_time_zone("Pacific Time (US & Canada)")..Time.zone.now)
+    @hours_month = @hours.where(updated_at: 1.month.ago.in_time_zone("Pacific Time (US & Canada)")..Time.zone.now)
+    @hw = 0
+    @hm = 0
+    for i in 0..@hours_week.size()-1
+      @hw += @hours_week[i].content
+    end
+    for i in 0..@hours_month.size()-1
+      @hm += @hours_month[i].content
+    end
   end
+  
+
 end
