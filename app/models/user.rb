0f8caee6848059b4256a7d6cd45e5af1502f53a7
Connor Harvey <conbonbot745@gmail.com>
2019-09-27 19:07:33 +0000

    yeah

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,10 +55,6 @@ class User < ApplicationRecord
     
     # Sends activation email
     def send_activation_email
-        if self.activation_token.nil?
-            self.activation_token = User.new_token
-            self.activation_digest = User.digest(activation_digest)
-        end
         UserMailer.account_activation(self).deliver_now
     end
     
