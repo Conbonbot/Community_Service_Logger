06e33c213865257beecf32bed62525464ae9d3c9
Connor Harvey <conbonbot745@gmail.com>
2019-09-29 21:38:24 +0000

    added breaks

--- app/controllers/account_activation_controller.rb
+++ app/controllers/account_activation_controller.rb
@@ -2,6 +2,7 @@ class AccountActivationController < ApplicationController
     
     def edit
         user = User.find_by(email: params[:email])
+        debugger
         if user && !user.activated? && user.authenticated?(:activation, params[:id])
             user.activate
             log_in user
