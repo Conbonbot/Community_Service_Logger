065429468c09093a6bfe5e5d12fe0f3c5abfd0c7
Connor Harvey <conbonbot745@gmail.com>
2020-03-02 20:09:27 +0000

    fixed?

---
+++ public/assets/HourDistributionChart-0c9420e2510193ec1f6f4622e0ec34a0f2c8f2daf2af9fb9e3ea9747aab2169d.js
@@ -0,0 +1,35 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+var hours = gon.distribution;
+var ctx = document.getElementById("HourDistributionChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Freshmen", "Sophomores", "Juniors", "Seniors"],
+    datasets: [{
+      data: hours,
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc', '#5d98f2'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
