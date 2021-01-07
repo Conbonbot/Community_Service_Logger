0634fd0b20049056533e91dc1c28c3b3784d2ce8
Connor Harvey <conbonbot745@gmail.com>
2021-01-06 19:47:41 -0800

    changed ):

--- app/controllers/account_activation_controller.rb
+++ app/controllers/account_activation_controller.rb
@@ -1,7 +1,6 @@
 class AccountActivationController < ApplicationController
     
     def edit
-        debugger
         user = User.find_by(email: params[:email])
         if user && !user.activated? && user.authenticated?(:activation, params[:id])
             user.activate
