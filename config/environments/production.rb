45b40e3d2f56c4d5bee4b6414ba5f9dd9a77bc90
Connor Harvey <conbonbot745@gmail.com>
2021-02-01 14:41:30 -0800

    hoppefully added changes to mailer that work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+
+  Rails.application.routes.default_url_options[:host] = "cv-community-service.herokuapp.com"
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
